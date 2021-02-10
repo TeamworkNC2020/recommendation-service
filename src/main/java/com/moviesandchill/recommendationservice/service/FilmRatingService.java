7f22f86d1adfefa04dcea8a086465189f4f02be4
weldnor <kolesov.anton.s@gmail.com>
2021-02-11 02:21:35 +0300

    Add FilmRatingService stub

---
+++ src/main/java/com/moviesandchill/recommendationservice/service/FilmRatingService.java
@@ -0,0 +1,9 @@
+package com.moviesandchill.recommendationservice.service;
+
+import com.moviesandchill.recommendationservice.entity.FilmRating;
+
+import java.util.List;
+
+public interface FilmRatingService {
+    List<FilmRating> getAllFilmsRatings();
+}
---
+++ src/main/java/com/moviesandchill/recommendationservice/service/impl/FilmRatingServiceImpl.java
@@ -0,0 +1,15 @@
+package com.moviesandchill.recommendationservice.service.impl;
+
+import com.moviesandchill.recommendationservice.entity.FilmRating;
+import com.moviesandchill.recommendationservice.service.FilmRatingService;
+import org.springframework.stereotype.Service;
+
+import java.util.List;
+
+@Service
+public class FilmRatingServiceImpl implements FilmRatingService {
+    @Override
+    public List<FilmRating> getAllFilmsRatings() {
+        return null;
+    }
+}
