3f2b69bdfdfa39e35f81ca7bb7a8cb6687dc2cb4
weldnor <kolesov.anton.s@gmail.com>
2021-04-02 17:41:48 +0300

    Add flyway

---
+++ src/main/resources/db/migration/V1_2__add_samples.sql
@@ -0,0 +1,54 @@
+INSERT INTO message_categorizer_samples (message_categorizer_training_sample_id, category, text)
+VALUES (1, 'greeting', 'привет !');
+INSERT INTO message_categorizer_samples (message_categorizer_training_sample_id, category, text)
+VALUES (2, 'greeting', 'добрый день !');
+INSERT INTO message_categorizer_samples (message_categorizer_training_sample_id, category, text)
+VALUES (3, 'greeting', 'здравствуйте !');
+INSERT INTO message_categorizer_samples (message_categorizer_training_sample_id, category, text)
+VALUES (4, 'greeting', 'привет ! как дела ?');
+INSERT INTO message_categorizer_samples (message_categorizer_training_sample_id, category, text)
+VALUES (5, 'conversation-complete', 'пока !');
+INSERT INTO message_categorizer_samples (message_categorizer_training_sample_id, category, text)
+VALUES (6, 'conversation-complete', 'до свидания !');
+INSERT INTO message_categorizer_samples (message_categorizer_training_sample_id, category, text)
+VALUES (7, 'conversation-complete', 'до встречи !');
+INSERT INTO message_categorizer_samples (message_categorizer_training_sample_id, category, text)
+VALUES (8, 'conversation-complete', 'еще увидимся !');
+INSERT INTO message_categorizer_samples (message_categorizer_training_sample_id, category, text)
+VALUES (9, 'conversation-complete', 'до связи !');
+INSERT INTO message_categorizer_samples (message_categorizer_training_sample_id, category, text)
+VALUES (10, 'conversation-complete', 'спасибо за все ');
+INSERT INTO message_categorizer_samples (message_categorizer_training_sample_id, category, text)
+VALUES (11, 'comedy', 'хочу посмотреть комедию');
+INSERT INTO message_categorizer_samples (message_categorizer_training_sample_id, category, text)
+VALUES (12, 'comedy', 'покажи комедии');
+INSERT INTO message_categorizer_samples (message_categorizer_training_sample_id, category, text)
+VALUES (13, 'comedy', 'хочу посмеяться ! покажи ');
+INSERT INTO message_categorizer_samples (message_categorizer_training_sample_id, category, text)
+VALUES (14, 'comedy', 'посоветуй комедию');
+INSERT INTO message_categorizer_samples (message_categorizer_training_sample_id, category, text)
+VALUES (15, 'comedy', 'какие посоветуешь комедии ?');
+INSERT INTO message_categorizer_samples (message_categorizer_training_sample_id, category, text)
+VALUES (16, 'action', 'хочу посмотреть боевик');
+INSERT INTO message_categorizer_samples (message_categorizer_training_sample_id, category, text)
+VALUES (17, 'action', 'покажи боевики');
+INSERT INTO message_categorizer_samples (message_categorizer_training_sample_id, category, text)
+VALUES (18, 'action', 'порекомендуй боевик');
+INSERT INTO message_categorizer_samples (message_categorizer_training_sample_id, category, text)
+VALUES (19, 'action', 'посоветуй боевик');
+INSERT INTO message_categorizer_samples (message_categorizer_training_sample_id, category, text)
+VALUES (20, 'action', 'какие посоветуешь боевики ?');
+INSERT INTO message_categorizer_samples (message_categorizer_training_sample_id, category, text)
+VALUES (21, 'romance', 'покажи фильмы про романтику');
+INSERT INTO message_categorizer_samples (message_categorizer_training_sample_id, category, text)
+VALUES (22, 'romance', 'порекомендуй романтические фильмы');
+INSERT INTO message_categorizer_samples (message_categorizer_training_sample_id, category, text)
+VALUES (23, 'romance', 'покажи фильмы про любовь');
+INSERT INTO message_categorizer_samples (message_categorizer_training_sample_id, category, text)
+VALUES (24, 'romance', 'покажи фильмы жанра романика');
+INSERT INTO message_categorizer_samples (message_categorizer_training_sample_id, category, text)
+VALUES (25, 'greeting', 'хай');
+INSERT INTO message_categorizer_samples (message_categorizer_training_sample_id, category, text)
+VALUES (26, 'romance', 'покажи романтику');
+INSERT INTO message_categorizer_samples (message_categorizer_training_sample_id, category, text)
+VALUES (27, 'action', 'пореккомендуй боевичок');
